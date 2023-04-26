5f2e3921e4f4f9acc40a4d3e13cc28af43b5f0b4
Tivadar Cziko <72433224+cziko1@users.noreply.github.com>
2023-04-26 10:45:27 +0200

    main.js
    1. fázis main menu, nagyon lassan haladok, nem jó

---
+++ app/main.js
@@ -0,0 +1,33 @@
+const myCustomCanvas = document.createElement('canvas');
+
+myCustomCanvas.id = 'myCustomCanvas';
+myCustomCanvas.style = 'border: 2px solid cyan';
+
+document.body.appendChild(myCustomCanvas);
+
+
+const config = {
+    type: Phaser.CANVAS,
+    width: 1200,
+    height: 720,
+    canvas: document.getElementById('myCustomCanvas'),
+    scene: {
+        preload: preload,
+        create: create
+    },
+    title: 'On the Uchiha Sasuke\'s ways '
+};
+
+const game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('pic', 'assets/uchiha_district.jpg');
+}
+
+function create ()
+{
+    this.add.image(600, 360, 'pic');
+
+    this.add.text(250, 100, ' '+game.config.gameTitle, {font: '50px Arial', fill: '#FF0000'});
+}
